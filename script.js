df00758de5a53397c392a4f0ddf323f47825f4ea
Guilherme P <gpelicaris@gmail.com>
2025-11-20 13:40:04 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode(){
+    const html = document.documentElement
+
+    /*if(html.classList.contains('light')){
+        html.classList.remove('light')
+    }else{
+        html.classList.add('light')
+    } oooooouuuuuu*/
+    html.classList.toggle('light')
+    /* tag toggle já faz o if else*/ 
+}
